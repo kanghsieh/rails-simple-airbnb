5b4e753fc0acb0a314b58ac6f70698e60329791a
Kang Hsieh <kanghsieh1990@gmail.com>
2022-02-16 16:13:29 +0100

    created seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
+
+10.times do
+  Flat.create!(
+    name: "#{Faker::Address.community} #{Faker::Address.city}",
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    description: Faker::Marketing.buzzwords,
+    price_per_night: rand(50..200),
+    number_of_guests: rand(1..5)
+  )
+end
